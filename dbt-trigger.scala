5e02917720cecc0d78bf7f567f6d135b96447467
Ryan Engel <rengel@technologypartners.net>
2023-10-18 11:10:07 -0500

    Adds trigger file

---
+++ dbt-trigger.scala
@@ -0,0 +1,28 @@
+// Databricks notebook source
+// MAGIC %sh
+// MAGIC pip install dbt-postgres
+
+// COMMAND ----------
+
+// MAGIC %sh
+// MAGIC pwd
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC cd dbt_demo_root
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC dbt deps --profiles-dir /dbfs/FileStore/ryan/
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC dbt seed --profiles-dir /dbfs/FileStore/ryan/
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC dbt run --profiles-dir /dbfs/FileStore/ryan/
